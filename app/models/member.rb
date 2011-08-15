4d0c8c4c826229d48197befaa92492d502d77de2
kendog <brian@kendog.freeserve.co.uk>
2011-08-15 13:46:05 +0100

    Removed non-git files

--- app/models/member.rb
+++
@@ -1,4 +0,0 @@
-class Member < ActiveRecord::Base
-  attr_accessible :name, :street, :town, :country, :postcode, :telenum, :email, :datejoin, :datepaid, :membertype, :amountpaid
-  validates_presence_of :name,:street,:town,:email,:postcode, :membertype
-end
--- app/models/member_fee.rb
+++
@@ -1,8 +0,0 @@
-class MemberFee < ActiveRecord::Base
-
-  attr_accessible :member_type, :fee
-
-  validates_uniqueness_of :member_type , :message => "You already have this name"
-  validates_numericality_of :fee
-  validates_presence_of :member_type, :fee
-end
